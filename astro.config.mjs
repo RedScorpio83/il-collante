b3fc308727a0ef7c61755ffba3c7ca7f47ef2e78
RedScorpio83 <acaliciotti@users.noreply.github.com>
2026-09-22 22:33:35 +0200

    chore: update site canonical domain to https://www.ilcollante.it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://il-collante.alessandro-caliciotti.workers.dev',
+  site: 'https://www.ilcollante.it',
 });
